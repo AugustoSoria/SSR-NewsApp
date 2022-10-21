e7100e10c1522709153eda95fee46d59275cba15
Augusto Soria <augustosoria@hotmail.es>
2022-10-21 20:40:05 -0300

    using useParams hook to get the slug and get the articles dependig of that, and adding a key to each articleCard component

--- src/client/components/ArticlesGrid.tsx
+++ src/client/components/ArticlesGrid.tsx
@@ -2,11 +2,15 @@ import React, { useEffect, useState } from "react";
 import { Article } from "../models/articles.model";
 import ArticleCard from "./ArticleCard";
 
+import { useParams } from 'react-router-dom';
+
 function ArticlesGrid() {
   const [articles, setArticles] = useState<Article[]>([])
+  let { slug = 'all'} = useParams();
+  console.log(slug)
 
   useEffect(() => {
-    fetch('api/articles')
+    fetch('/api/articles/' + slug)
       .then(response => response.json())
       .then(articles => setArticles(articles))
   }, [])
@@ -14,7 +18,7 @@ function ArticlesGrid() {
   return(
     <section className="ArticlesGrid">
       {
-        articles.map((a: Article) => <ArticleCard article={a}/>)
+        articles.map((a: Article) => <ArticleCard key={a._id} article={a}/>)
       }
     </section>
   )

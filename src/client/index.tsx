092bacc72978543906b187908eb9dfb7e284996b
Augusto Soria <augustosoria@hotmail.es>
2022-10-21 20:43:35 -0300

    getting and doing use of BrowserRouter

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./styles.css"
 
 const container = document.getElementById("root");
 if(container) {
   const root = ReactDOM.createRoot(container);
-  root.render(<App />)
+  root.render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  )
 }

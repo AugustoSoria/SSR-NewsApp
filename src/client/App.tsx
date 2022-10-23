e48055baf28b209b2be5f652fd7bfce2d304bdaf
Augusto Soria <augustosoria@hotmail.es>
2022-10-22 21:23:32 -0300

    importing the NavBar component and setting to the main tag the maxWidth1340 class

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,21 +1,22 @@
 import React from "react";
-import {
-  Route,
-  Routes,
-} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 import ArticlesGrid from "./components/ArticlesGrid";
+import NavBar from "./components/NavBar";
 import Tags from "./components/Tags";
 
 const App = () => {
   
   return (
     <>
-      <Tags />
-      <Routes>
-        <Route path="/" element={<ArticlesGrid />} />
-        <Route path="/tema/:slug" element={<ArticlesGrid />} />
-      </Routes>
+      <NavBar />
+      <main className="maxWidth1340">
+        <Tags />
+        <Routes>
+          <Route path="/" element={<ArticlesGrid />} />
+          <Route path="/tema/:slug" element={<ArticlesGrid />} />
+        </Routes>
+      </main>
     </>
   )
 }
